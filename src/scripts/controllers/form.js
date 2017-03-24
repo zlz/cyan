45ea0023804ee95246f7e3ef4599001c597bb411
zhlzh <zlz@live.cn>
2017-03-24 15:38:09 +0800

    模拟提交数据

--- src/scripts/controllers/form.js
+++ src/scripts/controllers/form.js
@@ -2,20 +2,19 @@
 angular.module('app')
     .controller('formCtrl', ['$rootScope', '$scope', 'bridge', 'crud', ($rootScope, $scope, bridge, crud) => {
         const path = bridge.GLOBAL_CONFIG.path;
-        let getData = () => {
-            crud.$http({
-                    method: 'GET',
-                    url: path + 'admin/form.do?lang=' + $rootScope.rootComm.trans
-                })
-                .then((res) => {
-                    if (res) {
-                        $scope.data = res.data.data;
-                    }
-                });
+        $scope.formSubmit = () => {
+            let req = $scope.formData;
+            if (req && $scope.form.$valid) {
+                crud.$http({
+                        method: 'post',
+                        url: path + 'admin/form.do?lang=' + $rootScope.rootComm.trans,
+                        data: req 
+                    })
+                    .then(() => {
+                        window.cyan.pop('成功提交！', 'success');
+                    });
+            }
         };
-        $rootScope.transWatch = $rootScope.$watch('rootComm.trans', () => {
-            getData();
-        });
         $scope.$on('$destroy', () => {
             if ($rootScope.transWatch instanceof Function) {
                 $rootScope.transWatch();
