d37f808398aa8fec0ecee658d0be2d0b85b890d1
zlz <zlz@live.cn>
2017-03-26 21:05:43 +0800

    首页内容填充

--- src/scripts/directives/ft.js
+++ src/scripts/directives/ft.js
@@ -7,9 +7,30 @@ angular.module('app')
             scope: {
                 data: '='
             },
-            template: `<div class="ft container-fluid">
-                <div class="pd-r-10 pd-l-2 text-right em-12">
-                    <a ng-href="{{data.dt.ft.href}}">{{data.dt.ft.text}}</a>
+            template: `<div class="ft em-12">
+                <div class="w-1140 container pd-b-20">
+                    <div class="row">
+                        <ul class="col-md-4 pd-t-20">
+                            <li>
+                                <img src="../../images/zlz.logo.png" width="46">
+                                <p class="mg-l-15 ibvm">
+                                    <a ng-href="{{data.dt.ft.href}}">{{data.dt.ft.text}}</a><br>
+                                    一个供自己测试和演示的网站
+                                </p>
+                            </li>
+                        </ul>
+                        <ul class="col-md-4 pd-t-20">
+                            <a href="https://github.com/zlz/cyan" target="_blank" title="Contribute on GitHub" class="ibvm mg-r-15" style="width:24px; height:24px;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 499.36"><path d="M256,6.32c-141.36,0-256,114.61-256,256,0,113.09,73.34,209,175.08,242.9,12.8,2.35,17.47-5.56,17.47-12.34,0-6.08-.22-22.18-.35-43.54C121,464.83,106,415,106,415c-11.64-29.57-28.42-37.45-28.42-37.45C54.31,361.71,79.31,362,79.31,362c25.69,1.81,39.21,26.38,39.21,26.38,22.84,39.12,59.92,27.82,74.5,21.27,2.33-16.54,8.94-27.82,16.25-34.22C152.43,369,92.67,347,92.67,248.94c0-27.95,10-50.8,26.35-68.69-2.63-6.48-11.42-32.5,2.51-67.75,0,0,21.49-6.88,70.4,26.24a242.65,242.65,0,0,1,128.18,0c48.87-33.13,70.33-26.24,70.33-26.24,14,35.25,5.18,61.27,2.55,67.75,16.41,17.9,26.31,40.75,26.31,68.69,0,98.35-59.85,120-116.88,126.32,9.19,7.9,17.38,23.53,17.38,47.41,0,34.22-.31,61.83-.31,70.23,0,6.85,4.61,14.81,17.6,12.31C438.72,471.29,512,375.4,512,262.34,512,120.94,397.37,6.32,256,6.32Z" transform="translate(0 -6.32)" fill="#333" fill-rule="evenodd"></path></svg> </a>
+                            <a target="_blank" class="ibvm mg-r-15" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=2ujv4u3t6_vvmqur9Lm1tw" style="overflow: hidden; height:24px; text-decoration:none;"><i class="icon iconfont icon-email" style="font-size:24px; color:#333; position:absolute; top:12px"></i></a><br>
+                            基于angular, sass, nodejs, gulp, webpack 等构建
+                        </ul>
+                        <ul class="col-md-4 pd-t-20">
+                            <li>浙ICP备14014127号-2</li>
+                            <li>
+                                <a class="ibvm" target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010402001010" style="text-decoration:none">浙公网安备 33010402001010号</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>`
         };
